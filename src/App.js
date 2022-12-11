661214b28a3418e7fc23f85a6406dcb2531f278f
Maciej Cywinski <cyvvilek@gmail.com>
2022-12-11 23:32:37 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
+import { useState } from 'react';
 import { 
   MapContainer,
   TileLayer,
-  GeoJSON
+  GeoJSON,
+  useMapEvents,
+  useMap,
+  Marker,
+  Popup
 } from 'react-leaflet';
 import data from './PL.json';
 import MinimapControl from './components/Minimap';
-import Logger from './components/Logger';
+// import Logger from './components/Logger';
 
-function App() {
-  return (
-    <MapContainer
-      style={{ width: '100%', height: '600px' }} 
-      center={[52, 19.4]}
-      zoom={6}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <MinimapControl position="topright" />
-      <Logger />
-      <GeoJSON data={data.features} />
-    </MapContainer>
+const Events = () => {
+  const [marker, setMarker] = useState(null);
+  const map = useMap();
+  useMapEvents({
+    click(e) {
+      console.log(e);
+      console.log(map);
+    },
+    contextmenu(e) {
+      setMarker(e.latlng)
+    }
+  })
+
+  return marker && (
+    <Marker position={marker}>
+      <Popup>You clicked here dumbass!</Popup>
+    </Marker>
   );
+
 }
+
+const App = () => (
+  <MapContainer
+    style={{ width: '100%', height: '600px' }} 
+    center={[52, 19.4]}
+    zoom={6}
+    zoomControl={false}
+    dragging={false}
+  >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <MinimapControl position="topright" />
+    {/* <Logger /> */}
+    <GeoJSON data={data.features} />
+    <Events />
+  </MapContainer>
+);
 
 export default App;
